5545774a6b93eba1067c401c2677fc1bdc87429b
ferguttz <carlosfer856@gmail.com>
2024-01-25 14:39:07 +0100

    first commit

---
+++ app/config/configDB.php
@@ -0,0 +1,6 @@
+<?php
+define('DB_SERVER','localhost:3307');
+define('DB_USER','root');
+define('DB_PASSWD','');
+define('DATABASE','TestMockaroo');
+
---
+++ app/views/list.php
@@ -0,0 +1,35 @@
+
+<form>
+<button type="submit" name="orden" value="Nuevo"> Cliente Nuevo </button><br>
+</form>
+<br>
+<?php $titulos = ['id','first_name','email','gender','ip_address','telefono']; ?>
+<table>
+<tr>
+    <?php foreach ($titulos as $valor): ?>
+        <th><a href="<?=$_SERVER['PHP_SELF'].'?ordenacion='.$valor?>"><?= $valor ?></a></th>
+    <?php endforeach ?>
+</tr>
+<?php foreach ($tvalores as $valor): ?>
+<tr>
+<td><?= $valor->id ?> </td>
+<td><?= $valor->first_name ?> </td>
+<td><?= $valor->email ?> </td>
+<td><?= $valor->gender ?> </td>
+<td><?= $valor->ip_address ?> </td>
+<td><?= $valor->telefono ?> </td>
+<td><a href="#" onclick="confirmarBorrar('<?=$valor->first_name?>',<?=$valor->id?>);" >Borrar</a></td>
+<td><a href="?orden=Modificar&id=<?=$valor->id?>">Modificar</a></td>
+<td><a href="?orden=Detalles&id=<?=$valor->id?>" >Detalles</a></td>
+
+<tr>
+<?php endforeach ?>
+</table>
+
+<form>
+<br>
+<button type="submit" name="nav" value="Primero"> << </button>
+<button type="submit" name="nav" value="Anterior"> < </button>
+<button type="submit" name="nav" value="Siguiente"> > </button>
+<button type="submit" name="nav" value="Ultimo"> >> </button>
+</form>
